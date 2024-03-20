d2a3e83aead73676d65dcda130f7bc543cced5c9
Mansi sharma <155693256+sharmam54@users.noreply.github.com>
2024-03-19 21:56:03 -0600

    updated

---
+++ sharmam54_Assignment2/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int sumEven = 0;
+        int sumOdd = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine($"Number {i} is even");
+                sumEven += i;
+            }
+            else
+            {
+                Console.WriteLine($"Number {i} is odd");
+                sumOdd += i;
+            }
+        }
+
+        Console.WriteLine($"Sum of even numbers: {sumEven}");
+        Console.WriteLine($"Sum of odd numbers: {sumOdd}");
+
+        Console.ReadKey();
+      
+    }
+}
